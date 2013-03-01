18b99eba19432e479d6064e87068d52cc0753107
Erick Zanardo <erickzanardo@ubuntu.ubuntu-domain>
2013-02-28 21:58:22 -0300

    Code Format

--- rambi-engine/src/main/java/com/rambi/core/RambiScriptMachine.java
+++ rambi-engine/src/main/java/com/rambi/core/RambiScriptMachine.java
@@ -21,127 +21,135 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 public class RambiScriptMachine {
-	public static RambiScriptMachine instance;
+    public static RambiScriptMachine instance;
 
-	private JsonParser parser = new JsonParser();
-	private JsonArray config;
-	private ScriptableObject global;
+    private JsonParser parser = new JsonParser();
+    private JsonArray config;
+    private ScriptableObject global;
 
-	public void init(String appConfig) {
-		config = parser.parse(readFile(appConfig)).getAsJsonArray();
+    public void init(String appConfig) {
+        config = parser.parse(readFile(appConfig)).getAsJsonArray();
 
-		Context cx = Context.enter();
+        Context cx = Context.enter();
 
-		global = new ImporterTopLevel(cx);
-		global.defineFunctionProperties(new String[] { "importModule" }, RambiScriptMachine.class,
-				ScriptableObject.DONTENUM);
+        global = new ImporterTopLevel(cx);
+        global.defineFunctionProperties(new String[] { "importModule" },
+                RambiScriptMachine.class, ScriptableObject.DONTENUM);
 
-		String services = RambiScriptMachine.readFile("com/rambi/core/service.js");
+        String services = RambiScriptMachine
+                .readFile("com/rambi/core/service.js");
 
-		Script servicesScript = cx.compileString(services, "services.js", 1, null);
-		servicesScript.exec(cx, global);
+        Script servicesScript = cx.compileString(services, "services.js", 1,
+                null);
+        servicesScript.exec(cx, global);
 
-		Context.exit();
-	}
+        Context.exit();
+    }
 
-	private RambiScriptMachine() {
-	}
+    private RambiScriptMachine() {
+    }
 
-	public static RambiScriptMachine getInstance() {
-		if (instance == null) {
-			instance = new RambiScriptMachine();
-		}
-		return instance;
-	}
+    public static RambiScriptMachine getInstance() {
+        if (instance == null) {
+            instance = new RambiScriptMachine();
+        }
+        return instance;
+    }
 
-	public void executeHttpRequest(HttpServletRequest req, HttpServletResponse resp) {
+    public void executeHttpRequest(HttpServletRequest req,
+            HttpServletResponse resp) {
 
-		String uri = req.getRequestURI();
-		String service = null;
+        String uri = req.getRequestURI();
+        String service = null;
 
-		for (JsonElement e : config) {
-			JsonObject o = (JsonObject) e;
-			String pattern = o.get("pattern").getAsString();
+        for (JsonElement e : config) {
+            JsonObject o = (JsonObject) e;
+            String pattern = o.get("pattern").getAsString();
 
-			if (uri.matches(pattern)) {
-				service = o.get("service").getAsString();
-			}
-		}
+            if (uri.matches(pattern)) {
+                service = o.get("service").getAsString();
+            }
+        }
 
-		if (service != null) {
-			Context cx = Context.enter();
-			String method = req.getMethod().toLowerCase();
+        if (service != null) {
+            Context cx = Context.enter();
+            String method = req.getMethod().toLowerCase();
 
-			String serviceScript = readFile(service);
+            String serviceScript = readFile(service);
 
-			try {
+            try {
 
-				ScriptableObject scriptableObject = (ScriptableObject) cx.newObject(global);
-				scriptableObject.setParentScope(global);
+                ScriptableObject scriptableObject = (ScriptableObject) cx
+                        .newObject(global);
+                scriptableObject.setParentScope(global);
 
-				// TODO cache this
-				Script script = cx.compileString(serviceScript, service, 1, null);
-				script.exec(cx, scriptableObject);
+                // TODO cache this
+                Script script = cx.compileString(serviceScript, service, 1,
+                        null);
+                script.exec(cx, scriptableObject);
 
-				Function f = (Function) global.get("doService");
-				f.call(cx, global, scriptableObject,
-						new Object[] {scriptableObject.get("service"), req, resp, method });
+                Function f = (Function) global.get("doService");
+                f.call(cx, global, scriptableObject, new Object[] {
+                        scriptableObject.get("service"), req, resp, method });
 
-			} catch (Exception e) {
-				if (e.getMessage().startsWith("unsupported")) {
-					throw new UnsupportedOperationException("Method " + method + " not allowed in this url "
-							+ req.getRequestURI());
-				} else {
-					throw new RuntimeException(e);
-				}
-			} finally {
-				Context.exit();
-			}
-		}
-	}
+            } catch (Exception e) {
+                if (e.getMessage().startsWith("unsupported")) {
+                    throw new UnsupportedOperationException("Method " + method
+                            + " not allowed in this url " + req.getRequestURI());
+                } else {
+                    throw new RuntimeException(e);
+                }
+            } finally {
+                Context.exit();
+            }
+        }
+    }
 
-	public static ScriptableObject importModule(Context cx, Scriptable thisObj, Object[] args, Function funObj) {
-		ScriptableObject object = null;
+    public static ScriptableObject importModule(Context cx, Scriptable thisObj,
+            Object[] args, Function funObj) {
+        ScriptableObject object = null;
 
-		if (args.length != 2) {
-			throw new RuntimeException("Must inform a path and module to import");
-		}
+        if (args.length != 2) {
+            throw new RuntimeException(
+                    "Must inform a path and module to import");
+        }
 
-		String path = (String) args[0];
-		String module = (String) args[1];
-		String file = RambiScriptMachine.readFile(path);
+        String path = (String) args[0];
+        String module = (String) args[1];
+        String file = RambiScriptMachine.readFile(path);
 
-		object = (ScriptableObject) cx.newObject(thisObj);
+        object = (ScriptableObject) cx.newObject(thisObj);
 
-		// TODO cache
-		Script script = cx.compileString(file, path, 1, null);
-		script.exec(cx, object);
+        // TODO cache
+        Script script = cx.compileString(file, path, 1, null);
+        script.exec(cx, object);
 
-		return (ScriptableObject)ScriptableObject.getProperty(object, module);
-	}
+        return (ScriptableObject) ScriptableObject.getProperty(object, module);
+    }
 
-	protected static String readFile(String file) {
-		BufferedReader br = null;
-		StringBuilder ret = new StringBuilder();
+    protected static String readFile(String file) {
+        BufferedReader br = null;
+        StringBuilder ret = new StringBuilder();
 
-		try {
-			InputStream resourceAsStream = RambiScriptMachine.class.getClassLoader().getResourceAsStream(file);
-			String line;
-			br = new BufferedReader(new InputStreamReader(resourceAsStream));
+        try {
+            InputStream resourceAsStream = RambiScriptMachine.class
+                    .getClassLoader().getResourceAsStream(file);
+            String line;
+            br = new BufferedReader(new InputStreamReader(resourceAsStream));
 
-			while ((line = br.readLine()) != null) {
-				ret.append(line).append(System.getProperty("line.separator"));
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (br != null)
-					br.close();
-			} catch (IOException ex) {
-				ex.printStackTrace();
-			}
-		}
-		return ret.toString();
-	}
+            while ((line = br.readLine()) != null) {
+                ret.append(line).append(System.getProperty("line.separator"));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null)
+                    br.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return ret.toString();
+    }
 }
--- rambi-engine/src/test/java/com/rambi/HttpAppTest.java
+++ rambi-engine/src/test/java/com/rambi/HttpAppTest.java
@@ -10,35 +10,35 @@ import org.junit.Test;
 import com.rambi.core.RambiScriptMachine;
 
 public class HttpAppTest {
-	private String appConfig = "com/rambi/HttpAppTestConfig.js";
+    private String appConfig = "com/rambi/HttpAppTestConfig.js";
 
-	@Before
-	public void init() {
-		RambiScriptMachine.getInstance().init(appConfig);
-	}
+    @Before
+    public void init() {
+        RambiScriptMachine.getInstance().init(appConfig);
+    }
 
-	@Test
-	public void testHttpRequest() {
-		HttpServletRequest req = new RequestMock("mock/mock", "GET") {
-			@Override
-			public String getParameter(String param) {
-				return param + " - Mock Value";
-			}
-		};
+    @Test
+    public void testHttpRequest() {
+        HttpServletRequest req = new RequestMock("mock/mock", "GET") {
+            @Override
+            public String getParameter(String param) {
+                return param + " - Mock Value";
+            }
+        };
 
-		ResponseMock responseMock = new ResponseMock();
-		RambiScriptMachine.getInstance().executeHttpRequest(req, responseMock);
+        ResponseMock responseMock = new ResponseMock();
+        RambiScriptMachine.getInstance().executeHttpRequest(req, responseMock);
 
-		assertEquals("param - Mock Valueparam - Mock Value\na a",
-				responseMock.getOutData());
-	}
+        assertEquals("param - Mock Valueparam - Mock Value\na a",
+                responseMock.getOutData());
+    }
 
-	@Test(expected = UnsupportedOperationException.class)
-	public void testHttpRequestUnsupported() {
-		HttpServletRequest req = new RequestMock("mock/mock", "DELETE");
+    @Test(expected = UnsupportedOperationException.class)
+    public void testHttpRequestUnsupported() {
+        HttpServletRequest req = new RequestMock("mock/mock", "DELETE");
 
-		ResponseMock responseMock = new ResponseMock();
-		RambiScriptMachine.getInstance().executeHttpRequest(req, responseMock);
-	}
+        ResponseMock responseMock = new ResponseMock();
+        RambiScriptMachine.getInstance().executeHttpRequest(req, responseMock);
+    }
 
 }
